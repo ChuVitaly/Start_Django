137cbbd80b9d405a42f48f6ddec41d5c5f19b0c6
Виталий Чухляев <vitaliy69@mail.ru>
2022-11-17 15:40:07 +0300

    Create handler

--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+import datetime
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('<h1>Hello from django</h1>')
+
+def time (request):
+    return HttpResponse(f'<h2>Time = {datetime.datetime.now().time()}</h2>')
